bbf87a036e82e8b4a15535421b70fe8936966654
Hugo Seigle <102899493+HugoSEIGLE@users.noreply.github.com>
2026-08-11 17:06:11 +0200

    Feat: Update CI and Deps (#17)
    * feat(ci): add codecov and update deps
    
    * feat(ci): add codecov and update deps

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    coverage: {
+      exclude: ['assets/dist/**', 'assets/tests/**'],
+      include: ['assets/src/**'],
+      provider: 'v8',
+      reporter: ['text', 'cobertura'],
+      reportsDirectory: 'coverage/javascript',
+    },
     environment: 'jsdom',
     include: ['assets/tests/**/*.test.js'],
     restoreMocks: true,
